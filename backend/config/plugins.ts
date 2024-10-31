60e89f0a903aa2094e92fb47a78f2925592c79e3
iFralex <61825057+iFralex@users.noreply.github.com>
2024-10-31 12:18:09 +0100

    Set correct firebase service account position

--- backend/config/plugins.ts
+++ backend/config/plugins.ts
@@ -10,7 +10,7 @@ module.exports = ({ env }) => ({
         config: {
             provider: "strapi-provider-firebase-storage",
             providerOptions: {
-                serviceAccount: require("../../FirebaseServiceAccount.json"),
+                serviceAccount: require("/etc/secrets/FirebaseServiceAccount.json"),
                 // Custom bucket name
                 bucket: env(
                     "STORAGE_BUCKET_URL"
