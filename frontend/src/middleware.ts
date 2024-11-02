16f4bf244a663abf5d2c58c30f9c5d71b262191c
iFralex <61825057+iFralex@users.noreply.github.com>
2024-11-02 17:58:50 +0100

    Redirect Auth test 4

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -7,6 +7,9 @@ const PRIVATE_PATHS = ["/dashboard"]
 
 export async function middleware(request: NextRequest) {
   if (request.nextUrl.pathname.includes("/__/auth/")) {
+    const url = request.nextUrl.clone()
+    url.basePath = "https://unica-3d18c.firebaseapp.com"
+    console.log(JSON.stringify(url))
     return NextResponse.redirect("https://unica-3d18c.firebaseapp.com/__/auth/" + request.nextUrl.pathname.split("/__/auth/")[1])
   }
 
